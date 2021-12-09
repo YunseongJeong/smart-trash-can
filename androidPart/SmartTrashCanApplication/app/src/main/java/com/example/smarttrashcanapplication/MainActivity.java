3dbf0224fb5a072b334f37c26c87d34c49824e18
mandu <jys0972@gmail.com>
2021-12-10 00:20:44 +0900

    connect firebase

--- androidPart/SmartTrashCanApplication/app/src/main/java/com/example/smarttrashcanapplication/MainActivity.java
+++ androidPart/SmartTrashCanApplication/app/src/main/java/com/example/smarttrashcanapplication/MainActivity.java
@@ -1,15 +1,25 @@
 package com.example.smarttrashcanapplication;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 
 import com.dinuscxj.progressbar.CircleProgressBar;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 public class MainActivity extends AppCompatActivity {
 
     private static final String DEFAULT_PATTERN="%d%%";
     CircleProgressBar circleProgressBar;
+
+    private FirebaseDatabase database;
+    private DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -17,7 +27,21 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         circleProgressBar = findViewById(R.id.circleFill);
-        circleProgressBar.setProgress(70);
+        circleProgressBar.setProgress(100);
+
+        database = FirebaseDatabase.getInstance();
+        databaseReference = database.getReference();
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.e("MainActivity", error.toException().toString());
+            }
+        });
     }
 
 
