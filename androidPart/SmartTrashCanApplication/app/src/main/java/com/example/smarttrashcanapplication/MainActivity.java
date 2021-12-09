8e508e2f49359726727f6d800d01cfe765f88323
mandu <jys0972@gmail.com>
2021-12-09 23:00:25 +0900

    updata android app
    add navigation menu
    add circle progress

--- androidPart/SmartTrashCanApplication/app/src/main/java/com/example/smarttrashcanapplication/MainActivity.java
+++ androidPart/SmartTrashCanApplication/app/src/main/java/com/example/smarttrashcanapplication/MainActivity.java
@@ -4,11 +4,21 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.dinuscxj.progressbar.CircleProgressBar;
+
 public class MainActivity extends AppCompatActivity {
+
+    private static final String DEFAULT_PATTERN="%d%%";
+    CircleProgressBar circleProgressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        circleProgressBar = findViewById(R.id.circleFill);
+        circleProgressBar.setProgress(70);
     }
+
+
 }
