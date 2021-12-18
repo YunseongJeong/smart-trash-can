eabe13b48be4499fbb2e048c057ca86da548cd33
mandu <jys0972@gmail.com>
2021-12-18 15:24:01 +0900

    add aduino2firebase by python

---
+++ aduino2firebase/aduino2firbase.py
@@ -0,0 +1,42 @@
+import serial
+
+import firebase_admin
+from firebase_admin import credentials
+from firebase_admin import db
+
+import time
+
+cred = credentials.Certificate("smart-trash-can-application-firebase-adminsdk-rz79l-53889be09d.json")
+firebase_admin.initialize_app(cred,{
+        'databaseURL' : 'https://smart-trash-can-application-default-rtdb.firebaseio.com/'
+    })
+
+
+PORT = 'COM3'
+BaudRate = 9600
+ARD= serial.Serial(PORT,BaudRate)
+
+def Decode(A):
+    A = A.decode()
+    A = str(A)
+    fillData = A[1:4]
+    airData = A[5:8]
+
+    dir = db.reference('Data/' +time.strftime('%Y%m%d%H%M', time.localtime(time.time())))
+    dir.update({'time' : time.strftime('%Y%m%d%H%M', time.localtime(time.time()))})
+    dir.update({'fillData' : fillData})
+    dir.update({'airData' : airData})
+                
+    return  fillData + ' ' + airData
+def Ardread(): # return list [Ard1,Ard2]
+
+    if ARD.readable():
+        LINE = ARD.readline()
+        code=Decode(LINE)
+        print(code)
+        return code
+    else :
+        print("읽기 실패 from _Ardread_")
+
+while (True):
+    Ardread()
