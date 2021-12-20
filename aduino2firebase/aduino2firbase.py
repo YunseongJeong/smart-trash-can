49488c248b651f9de0445cb28f38af101bf59ff7
mandu <jys0972@gmail.com>
2021-12-20 15:35:22 +0900

    add notification part (base)

--- aduino2firebase/aduino2firbase.py
+++ aduino2firebase/aduino2firbase.py
@@ -4,31 +4,50 @@ import firebase_admin
 from firebase_admin import credentials
 from firebase_admin import db
 
+from pyfcm import FCMNotification
+
 import time
 
+
+#for Noticitation
+APIKEY = "Your Server Key"
+TOKEN = "Your Token"
+push_service = FCMNotification(APIKEY)
+
+def sendMessage(body, title):
+    data_message = {
+        "body":body,
+        "title":title
+        }
+    result = push_service.single_device_data_message(registration_id=TOKEN, data_message=datamessage)
+    print(result)
+
+#for Database
 cred = credentials.Certificate("smart-trash-can-application-firebase-adminsdk-rz79l-53889be09d.json")
 firebase_admin.initialize_app(cred,{
         'databaseURL' : 'https://smart-trash-can-application-default-rtdb.firebaseio.com/'
     })
 
-
+#for Serial Comunication
 PORT = 'COM3'
 BaudRate = 9600
 ARD= serial.Serial(PORT,BaudRate)
+ 
+push_service = FCMNotification(APIKEY)
 
 def Decode(A):
     A = A.decode()
     A = str(A)
     fillData = A[1:4]
-    airData = A[5:8]
+    airData = A[5:9]
     timeData = time.strftime('%Y%m%d%H%M%S', time.localtime(time.time()))
-    dir = db.reference('Data/' + timeData)
+    dir = db.reference('Data/'+'20211219003103') #+ timeData)
     dir.update({'time' : timeData})
     dir.update({'fillData' : fillData})
     dir.update({'airData' : airData})
-                
     return  fillData + ' ' + airData
-def Ardread(): # return list [Ard1,Ard2]
+
+def Ardread():
 
     if ARD.readable():
         LINE = ARD.readline()
@@ -38,5 +57,10 @@ def Ardread(): # return list [Ard1,Ard2]
     else :
         print("읽기 실패 from _Ardread_")
 
+
+#Execution area
+sendMessage("for test", "test notification")
 while (True):
     Ardread()
+    
+
